dc2eb5e513a60ccd2150a8f76821895f85b6d0d9
yusei morimoto <yusei.morimoto@g.dreamcareer.co.jp>
2023-06-02 12:26:55 +0900

    AboutPageを新しく作って、コンポーネント化しました。

---
+++ components/Headline.jsx
@@ -0,0 +1,17 @@
+import styles from '../styles/Home.module.css'
+
+export function Headline(props) {
+    console.log(props);
+  return (
+    <div>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/{props.title}.js</code>
+        </p>
+    </div>
+  )
+}
